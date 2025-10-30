ffec9fbfc9a1a082f6ae1955e4ffc9ed2c39a522
aprcleav <aprcleaver@gmail.com>
2025-10-30 10:47:27 -0700

    Finished w1 Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
+                guessCount += 1;
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+
+            if (guess == number)
+            {
+                Console.WriteLine($"You guessed it! It took you {guessCount} guesses.");
+            }
+
+        Console.Write("Do you want to play again? (yes or no) ");
+        response = Console.ReadLine();
+        }
+        
     }
 }
