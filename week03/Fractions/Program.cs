d760fadf1d646d52406a7df69cc92f295c86673f
aprcleav <aprcleaver@gmail.com>
2025-11-12 13:15:27 -0800

    Finished W3 Encapsulation Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+
+        Console.WriteLine(f.GetTop());
+        Console.WriteLine(f.GetBottom());
+
+        f.SetTop(5);
+        f.SetBottom(9);
+
+        Console.WriteLine(f.GetTop());
+        Console.WriteLine(f.GetBottom());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
