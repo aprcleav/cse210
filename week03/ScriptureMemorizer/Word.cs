4762aea6e559e855d5dbe73c332fc3ea36f4ed8c
aprcleav <aprcleaver@gmail.com>
2025-11-13 17:44:50 -0800

    Stubs for Scripture Memorizer Program

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,31 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = true;
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+        return true;
+    }
+    
+    public string GetDisplayText()
+    {
+        return $"";
+    }
+}
